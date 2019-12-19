7e68c8573766ce476cab78c8481a82fb19bc409f
Flynn Doherty <flynndoh@gmail.com>
2019-12-19 22:52:47 +1300

    Added some new experiments

--- playground/js/files.js
+++ playground/js/files.js
@@ -1,8 +1,12 @@
 var files = {
     "math": [
-        "math_vectors_point-inside-triangle"
+        "math_vectors_point-inside-triangle",
+        "math_vectors_point-to-line"
     ],
     "boring": [
         "boring_rotating-cube"
+    ],
+    "inspiration": [
+        "inspiration_night-drive"
     ]
 };
